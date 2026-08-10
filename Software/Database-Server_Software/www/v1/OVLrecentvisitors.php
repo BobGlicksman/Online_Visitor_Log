84eeee8a14bf3075e6a4bfb99120652c41cc234f
Jim Schrempp <github1@jimschrempp.com>
2026-08-10 14:11:16 -0700

    added more fields to the OVL recent visitors page

--- Software/Database-Server_Software/www/v1/OVLrecentvisitors.php
+++ Software/Database-Server_Software/www/v1/OVLrecentvisitors.php
@@ -55,7 +55,7 @@ if (mysqli_connect_errno()) {
     //logfile("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
-$sql = "SELECT recNum, dateCheckinLocal, nameFirst, nameLast, email FROM ovl_visits "
+$sql = "SELECT recNum, dateCheckinLocal, nameFirst, nameLast, email, visitReason FROM ovl_visits "
         . " WHERE dateCheckinLocal between '" . $fiveDaysAgoSQL . "'"
         . "         AND '" . $nowSQL . "'"
         . " ORDER BY dateCheckinLocal DESC, nameLast ASC, nameFirst ASC"
@@ -72,15 +72,15 @@ if (!$result) {
 
     // create the divs
 
-    $outputTable = "<TABLE>"
-        . "<tr><th>Date</th><th>Time</th><th>Name</th><th>Email</th></tr>";
+    $outputTable = "<TABLE class='visitor-table'>"
+        . "<tr><th class='col-date'>Date</th><th class='col-time'>Time</th><th class='col-name'>Name</th><th class='col-email'>Email</th><th class='col-reason'>Reason</th></tr>";
     if (mysqli_num_rows($result) == 0) {
-        $outputTable .= "<tr><td colspan='4'>No visitors in last five days</td></tr>";
+        $outputTable .= "<tr><td colspan='5'><p class='no-visitors'>No visitors in last five days</p></td></tr>";
     } else {
         // loop over all rows
         while ($row = mysqli_fetch_assoc($result)) {
             //echo "row: " . $row["nameFirst"] . " " . $row["nameLast"] . "<br>";
-            $outputTable .= makeRow($row["dateCheckinLocal"], $row["nameFirst"], $row["nameLast"], $row["email"]);
+            $outputTable .= makeRow($row["dateCheckinLocal"], $row["nameFirst"], $row["nameLast"], $row["email"], $row["visitReason"]);
         }
     }
     $outputTable = $outputTable . "</TABLE>";
@@ -101,16 +101,18 @@ die;
 // Functions
 
 // make a row
-function makeRow($checkinDate, $nameFirst, $nameLast, $email) {
+function makeRow($checkinDate, $nameFirst, $nameLast, $email, $visitReason) {
 
     $date = substr($checkinDate, 0, 10);
     $time = substr($checkinDate, 11, 5);  // extract HH:MM from datetime
+    $timeDisplay = date("g:i A", strtotime($checkinDate));  // format as 3:45 PM
 
     $div = "<tr>"
-        . "<td>" . $date . "</td>"
-        . "<td>" . $time . "</td>"
-        . "<td style='padding:0 20px 0 0'>" . $nameFirst . "  " . $nameLast . "</td>"
+        . "<td>" . htmlspecialchars($date) . "</td>"
+        . "<td>" . $timeDisplay . "</td>"
+        . "<td>" . htmlspecialchars($nameFirst . " " . $nameLast) . "</td>"
         . "<td>" . htmlspecialchars($email) . "</td>"
+        . "<td>" . htmlspecialchars($visitReason) . "</td>"
         . "</tr>\r\n";
     return $div;
 }
